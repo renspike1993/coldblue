21ffa0fc789f1f46c0a9741d626fbecdde9aa2c1
renspike1993 <cold.renspike@gmail.com>
2026-06-20 10:27:38 +0800

    implement secured login

--- apps/admission/views.py
+++ apps/admission/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None and not user.is_staff:  # Only non-staff users
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'auth/login.html', {'error': 'Invalid login'})
+    
+    return render(request, 'auth/login.html')
